991c3434a80359c223092ca6b7349ecb80928b2b
Damian Ciasnocha <daneek80@gmail.com>
2020-06-07 21:16:52 +0100

    Add files via upload

---
+++ style.js
@@ -0,0 +1,14 @@
+// targetting button and popup
+const button = document.querySelector('button');
+const popup = document.querySelector('.popup-wrapper');
+const close = document.querySelector('.popup-close');
+
+button.addEventListener('click', () => {
+    popup.style.display = 'block';
+});
+close.addEventListener('click', () => {
+    popup.style.display = 'none';
+});
+popup.addEventListener('click', () => {
+    popup.style.display = 'none';
+});
